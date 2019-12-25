e51ecd608ecfb9524dcde8d543bfa509ed02d1c3
bad4iz <develop@bad4iz.ru>
2019-12-25 19:00:56 +0400

    connect db #7

--- server/app.js
+++ server/app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = 3005;
+
+mongoose.connect('mongodb+srv://bad4iz:23681@cluster0-0eruz.mongodb.net/test?retryWrites=true&w=majority', {useMongoClient: true});
 
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true,
 }));
+
+const dbConnection = mongoose.connection;
+dbConnection.on('error', err => console.log(`Connection error: ${err}`));
+dbConnection.once('open', () => console.log('Connected to DB!'));
 
 app.listen(PORT, err => console.log(err ? err : 'Server started'));
