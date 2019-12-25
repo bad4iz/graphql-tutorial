0cc094151192579632b871ca41b8728e9d1dab7f
bad4iz <develop@bad4iz.ru>
2019-12-25 19:08:42 +0400

    connect db #7

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,9 @@
 const graphql = require('graphql');
 
 const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
+
+const Movies = require('../models/movie');
+const Directors = require('../models/director');
 /*
 // All IDs set automatically by mLab
 // Don't forget to update after creation
@@ -49,7 +52,8 @@ const MovieType = new GraphQLObjectType({
     director: {
       type: DirectorType,
       resolve(parent, args) {
-        return directors.find(director => director.id == parent.id);
+        // return directors.find(director => director.id == parent.id);
+        return Directors.findById(parent.directorId);
       }
     }
   })
@@ -64,7 +68,8 @@ const DirectorType = new GraphQLObjectType({
     movies: {
       type: new GraphQLList(MovieType),
       resolve(parent, args) {
-        return movies.filter(movie => movie.directorId === parent.id);
+        // return movies.filter(movie => movie.directorId === parent.id);
+        return Movies.find({ directorId: parent.id });
       },
     },
   })
@@ -77,26 +82,30 @@ const Query = new GraphQLObjectType({
       type: MovieType,
       args: {id: {type: GraphQLID}},
       resolve(parent, args) {
-        return movies.find(movie => movie.id == args.id);
+        // return movies.find(movie => movie.id == args.id);
+        return Movies.findById(args.id);
       }
     },
     director: {
       type: DirectorType,
       args: {id: {type: GraphQLID}},
       resolve(parent, args) {
-        return directors.find(director => director.id == args.id);
+        // return directors.find(director => director.id == args.id);
+        return Directors.findById(args.id);
       }
     },
     movies: {
       type: new GraphQLList(MovieType),
       resolve(parent, args) {
-        return movies;
+        // return movies;
+        return Movies.find({});
       }
     },
     directors: {
       type: new GraphQLList(DirectorType),
       resolve(parent, args) {
-        return directors;
+        // return directors;
+        return Directors.find({});
       }
     }
   }
