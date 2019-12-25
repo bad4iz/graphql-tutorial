c478fda948e76ff7a96ba8fe4a70feb40f8043c5
bad4iz <develop@bad4iz.ru>
2019-12-25 11:38:53 +0400

    связь один ко многим

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,19 +1,23 @@
 const graphql = require('graphql');
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt} = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
 
 const movies = [
-  {id: '1', name: 'One', genre: 'Crime', directorId: '1'},
-  {id: '2', name: 'two', genre: 'Crime', directorId: '1'},
-  {id: 3, name: 'three', genre: 'a', directorId: '2'},
-  {id: 4, name: 'four', genre: 'Crime', directorId: '3'},
+  { id: '1', name: 'Pulp Fiction', genre: 'Crime', directorId: '1', },
+  { id: '2', name: '1984', genre: 'Sci-Fi', directorId: '2', },
+  { id: '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller', directorId: '3', },
+  { id: '4', name: 'Snatch', genre: 'Crime-Comedy', directorId: '4', },
+  { id: '5', name: 'Reservoir Dogs', genre: 'Crime', directorId: '1' },
+  { id: '6', name: 'The Hateful Eight', genre: 'Crime', directorId: '1' },
+  { id: '7', name: 'Inglourious Basterds', genre: 'Crime', directorId: '1' },
+  { id: '7', name: 'Lock, Stock and Two Smoking Barrels', genre: 'Crime-Comedy', directorId: '4' },
 ];
 
 const directors = [
-  {id: '1', name: 'super man', age: 44},
-  {id: '2', name: 'Michal', age: 4},
-  {id: '3', name: 'James ', age: 54},
-  {id: '4', name: 'Guy', age: 62},
+  { id: '1', name: 'Quentin Tarantino', age: 55 },
+  { id: '2', name: 'Michael Radford', age: 72 },
+  { id: '3', name: 'James McTeigue', age: 51 },
+  { id: '4', name: 'Guy Ritchie', age: 50 },
 ];
 
 const MovieType = new GraphQLObjectType({
@@ -37,6 +41,12 @@ const DirectorType = new GraphQLObjectType({
     id: {type: GraphQLID},
     name: {type: GraphQLString},
     age: {type: GraphQLInt},
+    movies: {
+      type: new GraphQLList(MovieType),
+      resolve(parent, args) {
+        return movies.filter(movie => movie.directorId === parent.id);
+      },
+    },
   })
 });
 
