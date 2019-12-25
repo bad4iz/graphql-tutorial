58961b28054b44cabf4f31ee8c0f557d3f457346
bad4iz <develop@bad4iz.ru>
2019-12-25 19:35:42 +0400

    mutation db #8

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,44 +4,6 @@ const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, G
 
 const Movies = require('../models/movie');
 const Directors = require('../models/director');
-/*
-// All IDs set automatically by mLab
-// Don't forget to update after creation
-const directorsJson = [
-  { "name": "Quentin Tarantino", "age": 55 }, // 5e0341b61c9d4400001f1eb0
-  { "name": "Michael Radford", "age": 72 }, // 5e03421d1c9d44000039da1d
-  { "name": "James McTeigue", "age": 51 }, // 5e0342321c9d44000039da1e
-  { "name": "Guy Ritchie", "age": 50 }, // 5e0342451c9d44000039da1f
-];
-// directorId - it is ID from the directors collection
-const moviesJson = [
-
-
-
-
-
-  ,
-
-];
-
-const movies = [
-  { id: '1', name: 'Pulp Fiction', genre: 'Crime', directorId: '1', },
-  { id: '2', name: '1984', genre: 'Sci-Fi', directorId: '2', },
-  { id: '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller', directorId: '3', },
-  { id: '4', name: 'Snatch', genre: 'Crime-Comedy', directorId: '4', },
-  { id: '5', name: 'Reservoir Dogs', genre: 'Crime', directorId: '1' },
-  { id: '6', name: 'The Hateful Eight', genre: 'Crime', directorId: '1' },
-  { id: '7', name: 'Inglourious Basterds', genre: 'Crime', directorId: '1' },
-  { id: '7', name: 'Lock, Stock and Two Smoking Barrels', genre: 'Crime-Comedy', directorId: '4' },
-];
-
-const directors = [
-  { id: '1', name: 'Quentin Tarantino', age: 55 },
-  { id: '2', name: 'Michael Radford', age: 72 },
-  { id: '3', name: 'James McTeigue', age: 51 },
-  { id: '4', name: 'Guy Ritchie', age: 50 },
-];
- */
 
 const MovieType = new GraphQLObjectType({
   name: 'Movie',
@@ -74,6 +36,43 @@ const DirectorType = new GraphQLObjectType({
     },
   })
 });
+
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addDirector: {
+      type: DirectorType,
+      args: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+      },
+      resolve(parent, args) {
+        const director = new Directors({
+          name: args.name,
+          age: args.age,
+        });
+        return director.save();
+      },
+    },
+    addMovie: {
+      type: MovieType,
+      args: {
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
+        directorId: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        const movie = new Movies({
+          name: args.name,
+          genre: args.genre,
+          directorId: args.directorId,
+        });
+        return movie.save();
+      },
+    }
+  }
+});
+
 
 const Query = new GraphQLObjectType({
   name: 'Query',
@@ -113,4 +112,5 @@ const Query = new GraphQLObjectType({
 
 module.exports = new GraphQLSchema({
   query: Query,
+  mutation: Mutation,
 });
