a85af43f75e92685475d75864e7723d053c8c291
bad4iz <develop@bad4iz.ru>
2019-12-25 19:51:58 +0400

    useNewUrlParser: #8

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = 3005;
 
-mongoose.connect('mongodb+srv://bad4iz:23681@cluster0-0eruz.mongodb.net/graphql-tutorial?retryWrites=true&w=majority', {useMongoClient: true});
+mongoose.connect('mongodb+srv://bad4iz:23681@cluster0-0eruz.mongodb.net/graphql-tutorial?retryWrites=true&w=majority', {useNewUrlParser: true});
 // mongoose.connect('mongodb://Yauhen:Pass123@ds163835.mlab.com:63835/graphql-tutorial', { useMongoClient: true });
 
 app.use('/graphql', graphqlHTTP({
