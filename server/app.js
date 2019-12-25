253e55cbe91c2a99d27e47304c85288ca357848b
bad4iz <develop@bad4iz.ru>
2019-12-25 22:30:01 +0400

    #11

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3005;
@@ -9,10 +10,13 @@ const PORT = 3005;
 mongoose.connect('mongodb+srv://bad4iz:23681@cluster0-0eruz.mongodb.net/graphql-tutorial?retryWrites=true&w=majority', {useNewUrlParser: true});
 // mongoose.connect('mongodb://Yauhen:Pass123@ds163835.mlab.com:63835/graphql-tutorial', { useMongoClient: true });
 
+app.use(cors());
+
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true,
 }));
+
 
 const dbConnection = mongoose.connection;
 dbConnection.on('error', err => console.log(`Connection error: ${err}`));
