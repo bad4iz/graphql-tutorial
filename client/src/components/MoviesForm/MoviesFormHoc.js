6d893fe5cb348614a686be9683c236364e94eb25
bad4iz <develop@bad4iz.ru>
2019-12-26 20:01:17 +0400

    update movie #15

--- client/src/components/MoviesForm/MoviesFormHoc.js
+++ client/src/components/MoviesForm/MoviesFormHoc.js
@@ -3,7 +3,7 @@ import { compose } from 'recompose';
 import {graphql} from 'react-apollo';
 
 
-import {addMovieMutation} from './mutation'
+import {addMovieMutation, updateMovieMutation} from './mutation'
 import {moviesQuery} from '../MoviesTable/queries'
 import {directorsQuery} from './queries.js'
 
@@ -18,4 +18,13 @@ const withGraphqlAdd = graphql(addMovieMutation, {
   }),
 });
 
-export default compose(withStyles(styles), withGraphqlAdd, graphql(directorsQuery) );
+const withGraphqlUpdate = graphql(updateMovieMutation, {
+  props: ({mutate}) => ({
+    updateMovie: movie => mutate({
+      variables: movie,
+      refetchQueries: [{query: moviesQuery}]
+    }),
+  }),
+});
+
+export default compose(withStyles(styles), withGraphqlAdd, withGraphqlUpdate, graphql(directorsQuery) );
